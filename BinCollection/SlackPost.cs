dda03c7323409cda91c164c5502e5519a68c8cf4
Stephen.Jones <stephen.jones@red-gate.com>
2022-12-02 16:03:24 +0000

    Only fire off the hook in December from 1st to 25th

--- BinCollection/SlackPost.cs
+++ BinCollection/SlackPost.cs
@@ -12,7 +12,7 @@ namespace BinCollection
 #if DEBUG
         private const string Schedule = "0 * * * * *";
 #else
-        private const string Schedule = "0 0 5 * * *";
+        private const string Schedule = "0 0 5 1-25 12 *";
 #endif
 
         [FunctionName("SlackPost")]
