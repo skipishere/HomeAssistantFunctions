78413199456d1f2842bf4959c0840f19b02fa8d8
Stephen.Jones <stephen.jones@red-gate.com>
2022-12-02 15:41:08 +0000

    Add slack poster test

---
+++ BinCollection/SlackPost.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace BinCollection
+{
+    public class SlackPost
+    {
+#if DEBUG
+        private const string Schedule = "0 * * * * *";
+#else
+        private const string Schedule = "0 * * * * *";
+#endif
+
+        [FunctionName("SlackPost")]
+        public static async Task Run([TimerTrigger(Schedule)]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            var client = new HttpClient()
+            {
+                BaseAddress = new Uri(Environment.GetEnvironmentVariable("Slack"))
+            };
+
+            var data = new { day = DateTime.UtcNow.Day.ToString() };
+            await client.PostAsJsonAsync(Environment.GetEnvironmentVariable("SlackWebHook"), data);
+        }
+    }
+}
