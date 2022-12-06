de56098bef21d5f205e9d5dadd349d3d09c40362
Stephen.Jones <stephen.jones@red-gate.com>
2022-12-06 13:48:55 +0000

    Add Advent of code puzzle title

--- BinCollection/SlackPost.cs
+++ BinCollection/SlackPost.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
@@ -14,19 +16,47 @@ namespace BinCollection
 #else
         private const string Schedule = "0 0 5 1-25 12 *";
 #endif
-
+        
         [FunctionName("SlackPost")]
         public static async Task Run([TimerTrigger(Schedule)]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
-            var client = new HttpClient()
+            using var client = new HttpClient()
             {
                 BaseAddress = new Uri(Environment.GetEnvironmentVariable("Slack"))
             };
 
-            var data = new { day = DateTime.UtcNow.Day.ToString() };
+            var puzzleTitle = await PuzzleTitle();
+            var data = new { day = DateTime.UtcNow.Day.ToString(), title = puzzleTitle };
             await client.PostAsJsonAsync(Environment.GetEnvironmentVariable("SlackWebHook"), data);
+        }
+
+        private static async Task<string> PuzzleTitle()
+        {
+            try
+            {
+                using var client = new HttpClient()
+                {
+                    BaseAddress = new Uri("https://adventofcode.com/"),
+                };
+
+                var userAgent = new ProductInfoHeaderValue("(+github.com/skipishere/HomeAssistantFunctions/blob/main/BinCollection/SlackPost.cs by me@stephenj.co.uk)");
+                client.DefaultRequestHeaders.UserAgent.Add(userAgent);
+
+                var date = DateTime.UtcNow.Date;
+                var address = $"{date.Year}/day/{date.Day}";
+
+                var response = await client.GetAsync(address);
+                var regex = new Regex("<article class=\"day-desc\"><h2>--- Day \\d+: (?<title>[\\w -]+) ---<\\/h2>");
+                var match = regex.Match(await response.Content.ReadAsStringAsync());
+                
+                return $" - {match.Groups["title"].Value}";
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
         }
     }
 }
